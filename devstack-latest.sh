728ead0f130ff2721ba761bf65f754857c3a8b0a
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-06-08 10:52:16 +0200

    Update devstack-latest.sh

--- devstack-latest.sh
+++ devstack-latest.sh
@@ -3,8 +3,6 @@
 DEBIAN_FRONTEND=noninteractive sudo apt-get -y update
 DEBIAN_FRONTEND=noninteractive sudo apt-get upgrade -y
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y crudini git
-externalip=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-novncproxy_base_url="http://$externalip:6080/vnc_auto.html"
 
 # Clone devstack repo
 
@@ -26,12 +24,12 @@ enable_service s-proxy s-object s-container s-account
 SWIFT_HASH=66a3d6b56c1f479c8b4e70ab5c2000f5
 SWIFT_REPLICAS=1
 SWIFT_DATA_DIR=$DEST/data/swift
-
-[[post-config|$NOVA_CPU_CONF]]
-[vnc]
-novncproxy_base_url=$novncproxy_base_url
 EOF
 
 ./stack.sh
+
+externalip=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+sudo crudini --set /etc/nova/nova-cpu.conf vnc novncproxy_base_url "http://$externalip:6080/vnc_auto.html"
+sudo systemctl restart devstack@n-cpu.service
 
 echo "You can access Horizon Dashboard at External IP address: http://$externalip/dashboard"
