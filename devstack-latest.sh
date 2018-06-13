caf3f63d9b352e84f8750c4d86bde83476ac81ff
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-06-13 13:42:52 +0200

    Update devstack-latest.sh

--- devstack-latest.sh
+++ devstack-latest.sh
@@ -2,8 +2,8 @@
 
 DEBIAN_FRONTEND=noninteractive sudo apt-get -y update
 DEBIAN_FRONTEND=noninteractive sudo apt-get upgrade -y
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y crudini git
 
+# Get Extarnal IP of this Instance
 externalip=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
 
 # Clone devstack repo
@@ -15,15 +15,16 @@ cd devstack
 # Prepare 'local.conf'
 cat <<- EOF > local.conf
 [[local|localrc]]
+# Set basic passwords
 ADMIN_PASSWORD=openstack
 DATABASE_PASSWORD=openstack
 RABBIT_PASSWORD=openstack
 SERVICE_PASSWORD=openstack
 # Configure Nova novnc Proxy Base URL with External IP of this Instance
 NOVNCPROXY_URL=http://$externalip:6080/vnc_auto.html
-
+# Enable Heat
 enable_plugin heat https://git.openstack.org/openstack/heat
-
+# Enable Swift
 enable_service s-proxy s-object s-container s-account
 SWIFT_HASH=66a3d6b56c1f479c8b4e70ab5c2000f5
 SWIFT_REPLICAS=1
